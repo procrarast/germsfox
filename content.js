a650dc1de4a2a4ffeba005314ea8f60d9a1ca89f
procrarast <procrarast@gmail.com>
2024-03-12 23:06:03 -0500

    Add blocklist/blocker GUI, general changes

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ var switcherWindowed;   // boolean which defines whether the switcher is tabbed 
 var switcherEnabled;    // boolean which defines whether the switcher is turned on
 //var switcherKeyUp;      // boolean which defines whether we want to send a feed keyup after switching tabs
 var usingTextBox = false;   // :chatting:
-var playerBlocklist;
+var playerBlocklist = [];
 //var skinBlocklist = []; // TODO
 
 //chrome.runtime.sendMessage("updateTabs");
@@ -27,7 +27,7 @@ var muteButton =            playerMenu.getElementsByClassName("userMenuItem")[1]
 var centerCard =            menuCenter.getElementsByClassName("card")[0];
 
 nickInit();
-updateSettings();
+updateAllSettings();
 //updateCustomSkinMenu();
 
 chatInput.setAttribute("maxlength", 100); // Increase max length of chat messages
@@ -57,14 +57,7 @@ chatObserver.observe(chatBox, {childList: true});
 const settingsButtonHTML =` 
 <button id="germsfoxButton" 
     type="button" 
-    style="
-        color: #e4c6e6; 
-        background-color: #382938; 
-        border: 2px solid #947995; 
-        margin-top: 10px; 
-        margin-left: 10px; 
-        width: 100%" 
-class="btn">
+    class="germsfox-btn">
     <img id="germsfoxIcon" alt="Icon" style="vertical-align: middle;"></img>
     <b> Germsfox</b>
 </button>`
@@ -83,17 +76,70 @@ const settingsModalHTML = `
         <input type="checkbox" id="windowedCheckbox"> 
         <label for="windowedCheckbox">Windowed mode</label><br> 
 
-        <div id = "keyTester" style ="border: 2px solid #1e1a1e; border-radius: 4px; padding: 4px; cursor: pointer; text-align: center; width: 64px; height: 32px;">
+        <div id="keyTester" class="key-tester">
         </div> 
+        <button id="blocklistButton" 
+            type="button" 
+            class="germsfox-btn">
+            <b>Edit Blocklist</b>
+        </button>
+    </div>
+ </div>`;
+
+const customSkinsContainerHTML = `
+<div id="customSkinList"</div>`;
+
+const blocklistHTML = `
+<div id="blocklistModal" class="germsfox-modal">
+    <div class="germsfox-modal-content">
+        <span id="blocklistClose" class="germsfox-close">&times;</span>
+        <div style="display: flex; justify-content: space-between; align-items: left; margin-bottom: 20px;">
+            <h2><b>Blocklist</b></h2>
+            <button type="button" id="addBlockButton" class="germsfox-btn" style="margin-top: 0px; margin-right: 60px; padding: 0px; height: 40px; width: 40px";><b>+</b></button>
+        </div>
+        <ul id="blocklistList"></ul>
     </div>
 </div>`;
 
-const customSkinsContainerHTML = `
-<div id="customSkinList" style="margin-bottom: 10px;">
+const blockerHTML = `
+<div id="blockerModal" class="germsfox-modal">
+    <div class="germsfox-modal-content">
+        <span id="blockerClose" class="germsfox-close">&times;</span>
+        <h2><b>Blocker</b></h2>
+        <ul id="blockerList"></ul>
+    </div>
 </div>`;
-
 const germsfoxStyle = `
 <style>
+    .germsfox-btn {
+        color: #e4c6e6; 
+        background-color: #382938; 
+        border: 2px solid #947995; 
+        margin-top: 10px; 
+        margin-left: 10px; 
+        height: 40px;
+        width: 100%;
+        cursor: pointer;
+        text-align: center;
+        vertical-align: middle;
+        user-select: none;
+        padding: 0.375rem 0.75rem;
+        border-radius: 0.25rem;
+        transition: color 0.3s ease-in-out;
+    }
+    .germsfox-btn:hover {
+        background-color:
+    }
+    .key-tester {
+        border: 2px solid #1e1a1e; 
+        border-radius: 4px; 
+        padding: 4px; 
+        cursor: pointer;
+        text-align: center; 
+        width: 64px; 
+        height: 32px; 
+        user-select: none;
+    }
     .germsfox-modal {
         display: none;
         position: fixed;
@@ -106,7 +152,6 @@ const germsfoxStyle = `
         background-color: rgba(0, 0, 0, 0.4);
     }
     .germsfox-modal-content {
-        font-family: Ubuntu;
         background-color: #382938;
         color: #e4c6e6;
         margin: 15% auto;
@@ -120,10 +165,11 @@ const germsfoxStyle = `
         float: right;
         font-size: 28px;
         font-weight: bold;
+        user-select: none;
     }
     .germsfox-close:hover,
     .germsfox-close:focus {
-        color: black;
+        color: white;
         text-decoration: none;
         cursor: pointer;
     }
@@ -133,18 +179,42 @@ const germsfoxStyle = `
         color: white;
         display: none;
     }
+    #blocklistList {
+        user-select: none;
+        cursor: pointer;
+    }
+    #blocklistList li:hover {
+        text-decoration: line-through;
+        color: red;
+        transition: color 0.3s ease-in-out;
+    }
+    #blockerList {
+        user-select: none;
+        cursor: pointer;
+    }
+    #blockerList li:hover {
+        color: white;
+        transition: color 0.3s ease-in-out;
+    }
 </style>
 `;
 
 settingsButton.insertAdjacentHTML('afterend', settingsButtonHTML);
 customSkinsElement.insertAdjacentHTML('beforeend', customSkinsContainerHTML);
 document.body.insertAdjacentHTML('beforeend', settingsModalHTML);
+document.body.insertAdjacentHTML('beforeend', blocklistHTML);
+document.body.insertAdjacentHTML('beforeend', blockerHTML);
 document.head.insertAdjacentHTML('beforeend', germsfoxStyle);
 
-// this is for the icon on the germsfox button
-var germsfoxIcon = document.getElementById('germsfoxIcon');
-germsfoxIcon.src = chrome.runtime.getURL('images/gsDuhFox-19.png');
-    
+var addBlockButton =        document.getElementById("addBlockButton");
+var blockerModal =          document.getElementById("blockerModal");
+var blockerMenu =           document.getElementById("blockerMenu");
+var blockerCloseButton =    document.getElementById("blockerClose");
+var blockerList =           document.getElementById("blockerList");
+var blocklistButton =       document.getElementById("blocklistButton");
+var blocklistModal =        document.getElementById("blocklistModal");
+var blocklistCloseButton =  document.getElementById("blocklistClose");
+var blocklistList =         document.getElementById("blocklistList");
 var germsfoxButton =        document.getElementById("germsfoxButton");
 var settingsModal =         document.getElementById("germsfoxSettingsModal");
 var keyTester =             document.getElementById("keyTester");
@@ -153,7 +223,12 @@ var windowedCheckbox =      document.getElementById("windowedCheckbox");
 var settingsCloseButton =   document.getElementById("germsfoxSettingsClose");
 var customSkinsContainer =  document.getElementById("customSkinList");
 var applySkinButton =       document.querySelector("#customSkin .btn-info");
+var germsfoxIcon =          document.getElementById('germsfoxIcon');
 
+germsfoxIcon.src = chrome.runtime.getURL('images/gsDuhFox-19.png');
+
+addBlockButton.addEventListener('click', openBlockerMenu);
+blocklistButton.addEventListener('click', openBlocklistMenu);
 applySkinButton.addEventListener('click', customSkinSubmitted);
 skinsButton.addEventListener('click', updateCustomSkinMenu);
 enabledCheckbox.addEventListener('change', checkboxChanged);
@@ -166,12 +241,93 @@ germsfoxButton.addEventListener('click', function() {
         document.getElementById('enabledCheckbox').checked = items.switcherEnabled;
         document.getElementById('windowedCheckbox').checked = items.switcherWindowed;
     });
-    settingsModal.style.display = "block"; // un-hide settings modal
+    settingsModal.style.display = "block";
+});
+
+function openBlockerMenu() {
+    updateBlockerMenu();
+    blocklistModal.style.display = "none";
+    blockerModal.style.display = "block";
+}
+
+function closeBlockerMenu() {
+    blocklistModal.style.display = "block";
+    blockerModal.style.display = "none";
+}
+
+function openBlocklistMenu() {
+    stopWaiting();
+    settingsModal.style.display = "none";
+    
+    updateBlocklistMenu();
+    blocklistModal.style.display = "block";
+}
+
+function getChatNames(amount) {
+    const chatNamesList = [];
+    
+    for (const chatMessage of chatBox.children) {
+        const chatterNameElement = chatMessage.querySelector('b');
+        if (chatterNameElement) {
+            const chatterName = chatterNameElement.textContent;
+            if (!chatNamesList.includes(chatterName)) {
+                chatNamesList.unshift(chatterName);
+                if (chatNamesList.length >= amount) break;
+            }
+        }
+    }
+    return chatNamesList;
+}
+
+function updateBlockerMenu() {
+    blockerList.innerHTML = "";
+    const chatters = getChatNames(10);
+
+    if(chatters.length > 0) {
+        const messageItem = document.createElement('p');
+        messageItem.textContent = "Click a player to block them. Get that ### banned.";
+        blockerList.appendChild(messageItem);
+
+        for (let i = 0; i < chatters.length; i++) {
+            const listItem = document.createElement('li');
+            listItem.textContent = chatters[i];
+            listItem.style.cursor = 'pointer'; // Ensure it looks clickable
+        
+            listItem.addEventListener('click', function onClick() {
+                console.log('Blocked player', chatters[i]);
+                blockPlayerName(chatters[i]);
+                chrome.storage.local.set({ "playerBlocklist": playerBlocklist });
+        
+                listItem.style.textDecoration = 'line-through';
+                listItem.style.color = 'darkgray';
+                listItem.style.cursor = 'default';
+        
+                listItem.removeEventListener('click', onClick);
+            });
+        
+            blockerList.appendChild(listItem);
+        }
+    } else {
+        const messageItem = document.createElement('p');
+        messageItem.textContent = "Nobody's chatted yet! Quite the rarity. Consider yourself lucky.";
+        blockerList.appendChild(messageItem);
+    } 
+}
+
+blocklistCloseButton.addEventListener('click', function() {
+    blocklistModal.style.display = "none";
+    settingsModal.style.display = "block";
+});
+
+blockerCloseButton.addEventListener('click', function() {
+    updateBlocklistMenu();
+    blocklistModal.style.display = "block";
+    blockerModal.style.display = "none";
 });
 
 settingsCloseButton.addEventListener('click', function() {
     stopWaiting();
-    settingsModal.style.display = "none"; // hide settings modal 
+    settingsModal.style.display = "none";
 });
 
 keyTester.addEventListener('click', function() {
@@ -201,14 +357,10 @@ muteButton.addEventListener('click', function() {
     }
     console.log(playerBlocklist);
     chrome.storage.local.set({ "playerBlocklist": playerBlocklist }, function() {
-        updateSettings();
-    })
-
+        updateAllSettings();
+    });
+    chatBox.scrollTop = chatBox.scrollHeight;
 });
-
-function unblockPlayerName(playerName) {
-    playerBlocklist.splice(playerBlocklist.indexOf(playerName), 1); 
-}
 
 function checkboxChanged() {
     const switcherEnabled = document.getElementById('enabledCheckbox').checked;
@@ -216,8 +368,12 @@ function checkboxChanged() {
     
     chrome.storage.local.set({ "switcherEnabled": switcherEnabled, "switcherWindowed": switcherWindowed }, function() {
         console.log('Settings saved');
-        updateSettings();
+        updateAllSettings();
     });
+}
+
+function unblockPlayerName(playerName) {
+    playerBlocklist.splice(playerBlocklist.indexOf(playerName), 1); 
 }
 
 function blockPlayerName(playerName) {
@@ -253,7 +409,7 @@ function stoppedUsingTextBox() {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "updateSettings") {
         console.info("Updating settings");
-        updateSettings();
+        updateAllSettings();
     }
 });
 
@@ -262,6 +418,15 @@ window.onclick = function(event) {
     if (event.target === settingsModal) {
         stopWaiting();
         settingsModal.style.display = "none";
+    }
+    else if (event.target === blocklistModal) {
+        blocklistModal.style.display = "none";
+        settingsModal.style.display = "block";
+    }
+    else if (event.target === blockerModal) {
+        updateBlocklistMenu();
+        blockerModal.style.display = "none";
+        blocklistModal.style.display = "block";
     }
 };
 
@@ -274,7 +439,7 @@ function keydown(event) {
         
         chrome.storage.local.set({ "switcherKey": switcherKey }, function() {
             console.log('Switcher key changes saved');
-            updateSettings();
+            updateAllSettings();
         });
         stopWaiting();
     }
@@ -293,7 +458,7 @@ function keydown(event) {
     } */
 }
 
-function updateSettings() {
+function updateAllSettings() {
     // germs.io settings
     const settings = localStorage.getItem('settings');
     if (settings) {
@@ -366,6 +531,29 @@ function updateCustomSkinMenu() {
     for (i = 0; i < customSkins.length; i++) {
         var imgHTML = `<li id="skinSkin"><img onContextMenu="showDeleteButton(event, ${i})" onclick="setSkin('${customSkins[i]}');" class="lazy loaded" width="84" height="85" src="${customSkins[i]}"></li>`;
         customSkinsContainer.innerHTML += imgHTML
+    }
+}
+
+function updateBlocklistMenu() {
+    blocklistList.innerHTML = "";
+
+    if(playerBlocklist.length > 0) {
+        for(let i = 0; i < playerBlocklist.length; i++) {
+            const listItem = document.createElement('li');
+            listItem.textContent = playerBlocklist[i];
+            
+            listItem.addEventListener('click', function() {
+                console.log('Removing blocked player', playerBlocklist[i]);
+                unblockPlayerName(playerBlocklist[i]);
+                updateBlocklistMenu();
+                chrome.storage.local.set({ "playerBlocklist": playerBlocklist });
+            });
+            blocklistList.appendChild(listItem);
+        }
+    } else {
+        const messageItem = document.createElement('p');
+        messageItem.textContent = "You have nobody blocked. Such a tolerant creature!";
+        blocklistList.appendChild(messageItem);
     }
 }
 
